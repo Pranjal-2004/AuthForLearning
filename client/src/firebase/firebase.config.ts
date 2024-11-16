7499162f9b91fa3c27c633ff43e0ce8042771efd
Pranjal Bhagat <117507692+Pranjal-2004@users.noreply.github.com>
2024-11-16 11:22:28 +0530

    Update firebase.config.ts

--- client/src/firebase/firebase.config.ts
+++ client/src/firebase/firebase.config.ts
@@ -4,13 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmm5qBrTHwsMNZbtQNtqNwF8mkr8JwoUM",
-  authDomain: "learning-app-8ea80.firebaseapp.com",
-  projectId: "learning-app-8ea80",
-  storageBucket: "learning-app-8ea80.firebasestorage.app",
-  messagingSenderId: "561632208199",
-  appId: "1:561632208199:web:f67937cee3772ca8b45862",
-  measurementId: "G-H41BCD196B"
+  // write your config
 };
 
 const app = initializeApp(firebaseConfig);
